7ed2c2643efec696fd2b8dc7f13ca709689b85d7
Fadhlallah Almohammed <2230006097@iau.edu.sa>
2024-02-08 17:42:34 +0300

    Completed task 1

---
+++ task1.py
@@ -0,0 +1,10 @@
+def print_staircase(n):
+    for i in range(1, n + 1):
+        print(' ' * (n - i) + '#' * i, end='')
+        print('  ', end='')
+        print('#' * i)
+
+
+n = int(input("Enter the size of the staircase: "))
+
+print_staircase(n)
