356d90d6731f2e16ae2b6279d7563c6592628739
sarraa22 <saraaldossari96@gmail.com>
2024-02-16 03:03:35 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,9 @@
+size = int(input("please enter the staircase size (positive number) : "))#user enters size
+
+def printS(size):
+    for i in range(1, size + 1):
+        {
+             print('*' * i)
+        }
+printS(size)#call function
+    
